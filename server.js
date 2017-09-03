a7e04dfd608e0011bee1b0ccfb775eb53d9f44eb
sheethal3838 <31582940+sheethal3838@users.noreply.github.com>
2017-09-03 17:33:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article one', function (req, res) {
+app.get('/article-one', function (req, res) {
     res.send("article one coming soon");
-}
+});
 
     app.get('/article two', function (req, res) {
     res.send("article two coming soon");
-}
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
